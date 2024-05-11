85a5f826075e646ee58e5ae44c443b4b63dc231a
akhilbz <akhilesh.bitla@gmail.com>
2024-05-11 03:23:25 -0500

    Saving Work

--- src/backend/contacts_backend/contacts/models.py
+++ src/backend/contacts_backend/contacts/models.py
@@ -3,16 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-    prefix = models.CharField(max_length=5)
-    first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    job_title = models.CharField(max_length=150)
-    company = models.CharField(max_length=150)
-    photo_url = models.CharField(max_length=2000)
-    phone_no = models.CharField(max_length=14)
-    home_no = models.CharField(max_length=14)
-    mobile_no = models.CharField(max_length=14)
-    email = models.CharField(max_length=300)
-    website_url = models.CharField(max_length=2000)
+    full_name = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    photo_path = models.TextField()
+    phone_no = models.JSONField()
+    email = models.JSONField()
     
